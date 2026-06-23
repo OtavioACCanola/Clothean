3307e8c99605982182cdc857474d0d4e215a06ea
OTAVIO AUGUSTO CANOLA DO CARMO <otavio.accarmo@senacsp.edu.br>
2026-06-23 14:26:09 -0300

    enviando alterações consultar usuário

--- JavaScript/FrontEnd/Controller/ConsultarUsuarioComum.js
+++ JavaScript/FrontEnd/Controller/ConsultarUsuarioComum.js
@@ -15,32 +15,30 @@ const txtSenha2 = document.getElementById("TxtSenha");
 function consultaUsuario() {
 
     // linhaAluno.textContent = ""; // Limpa tudo antes de adicionar os alunos do json
-    const token =  localStorage.getItem("token")
+    const token = localStorage.getItem("token")
     const usuarioLogado = jwt_decode(token)
     const id = usuarioLogado.id
 
-    obterUsuariosEmail(id).then((listaUsuario) => { // Vai pegar a lista Json obtida pelo método da model
-        console.log(listaUsuario)
-        if (listaUsuario.length === 0) {
+    obterUsuariosEmail(id).then((Usuario) => { // Vai pegar a lista Json obtida pelo método da model
+        console.log(Usuario)
+        if (Usuario=== null) {
             mensagemErro("Não tem nenhum Usuario cadastrado no servidor ainda!");
         }
         else {
-            listaUsuario.forEach(function (usuario) { // Percorre a lista obtida da Model
-                btnEditarUsu.dataset.id = usuario.id,
-                btnExcluirUsu.dataset.id = usuario.id,
-                txtNome.value = usuario.nome, 
-                txtEmail.value = usuario.email, 
-                txtCPF.value = usuario.cpf, 
-                txtTelefone.value = usuario.telefone, 
-                txtPerfil.value = usuario.perfil
+            btnEditarUsu.dataset.id = Usuario.id;
+                btnExcluirUsu.dataset.id = Usuario.id;
+                txtNome.value = Usuario.nome;
+                txtEmail.value = Usuario.email;
+                txtCPF.value = Usuario.cpf;
+                txtTelefone.value = Usuario.telefone;
+                txtPerfil.value = Usuario.perfil;
 
-                dadosOriginaisUsuario = {
-                    nome: usuario.nome,
-                    email: usuario.email,
-                    cpf: usuario.cpf,
-                    telefone: usuario.telefone
-                }
-            })
+            dadosOriginaisUsuario = {
+                nome: Usuario.nome,
+                email: Usuario.email,
+                cpf: Usuario.cpf,
+                telefone: Usuario.telefone
+            }
         }
     });
 };
