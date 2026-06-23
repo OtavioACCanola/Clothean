f2331bbbae34123b2f340c9b946233034e3d2d32
OTAVIO AUGUSTO CANOLA DO CARMO <otavio.accarmo@senacsp.edu.br>
2026-06-23 16:27:08 -0300

    enviando alterações pesquisar usuário

--- JavaScript/BackEnd/modelService/service.js
+++ JavaScript/BackEnd/modelService/service.js
@@ -47,7 +47,7 @@ exports.editarAdm = (usuario, callback) => {
 exports.loginUsuario = (usuario,  callback) => {
     console.log("--> 1. Dados recebidos no Service:", usuario);
 
-    repositories.buscaEmail(usuario.email, (usuarioEncontrado) => {
+    repositories.buscaEmail(usuario, (usuarioEncontrado) => {
         console.log("--> 2. Retorno do Banco de Dados:", usuarioEncontrado);
 
         if (!usuarioEncontrado) {
