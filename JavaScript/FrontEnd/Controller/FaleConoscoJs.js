154b72bb93275b56bd9c7a52eedb6461556bbea6
Otávio Augusto Canola do Carmo <otavio.augusttocc@gmail.com>
2026-06-24 16:35:44 -0300

    corrigindo a importação

--- JavaScript/FrontEnd/Controller/FaleConoscoJs.js
+++ JavaScript/FrontEnd/Controller/FaleConoscoJs.js
@@ -1,7 +1,7 @@
 
 // Colocar as chaves no arquivo .env (Pesquisar)
 // Consertar clear() quando enviar mensagem
-import { CONFIG } from "./config";
+import { CONFIG } from "../Controller/config";
 
 const enviar = document.getElementById("enviar")
 const form = document.getElementById("formularioContato")
